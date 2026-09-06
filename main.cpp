0b58639b11fb87d1e54085744b12490796de5a25
suhAnton1934 <onton_vrn@mail.ru>
2026-09-06 12:18:19 +0300

    lalala

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main(){
     cout << "Hello children" << endl;
+    int a, count = 0;
+    cin >> a;
 
+    while (a != 0){
+        count += 1;
+        a = a / 10;
+    }
+    cout << count << endl;
     return 0;
 }
